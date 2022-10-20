9d93487285fa335bacabc0b83f8522a7fa544605
Cansu Öztaş <fcansuoztas@gmail.com>
2022-10-20 13:29:48 +0300

    js

--- src/Assets.js
+++ src/Assets.js
@@ -9,30 +9,33 @@ const Assets = ({user,getUser}) => {
   const{name,phone,location,picture,email,dob,registered}=user
   // console.log(name)
   return (
-
+<> 
     <div className='container'>
         <div className='ilk'>
-        <img src={picture?.large} alt="" />
+        <img className='image' src={picture?.large} alt="" />
         <h2>{name?.title}{name?.first}</h2>
         </div>
         <div className='ilk'>
-        <img src={Mail}  width="25px" alt="" /> 
+        <img src={Mail}  width="30px"  /> 
         <p>{email}</p>
         </div>
         <div className='ilk '>
-        <img src={Phone} width="25px" alt="" />
+        <img src={Phone} width="30px" alt="" />
         <p> {phone}</p></div>
         <div className='ilk'>
-        <img src={Location} width="25px" alt="" />
+        <img src={Location} width="30px" alt="" />
          <p>{location?.state} {location?.country}</p>
          </div>
+         
     <div className='footer'>
      <p>Age: {dob?.age}</p>
      <br />
      <p>Registered Date: {registered?.date.slice(0,10)}</p>
     </div>
+   
+    </div> 
     <button onClick={getUser}>Random User</button>
-    </div>
+  </>
   )
 }
 
